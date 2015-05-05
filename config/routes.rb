0c9ee826725192115edd1675e58bccbf33716cab
Jeremy Park <jeremyjpark@gmail.com>
2015-05-04 21:24:31 -0700

    devise working?

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :listings
+
+
+  root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
